86d2b429ddb34a6b319b6738480728c0daac3ab9
Francisco-Meza <86315912+Francisco-Meza@users.noreply.github.com>
2022-03-25 14:40:20 -0700

    Agregue el proyecto

---
+++ LOGICA DE NEGOCIOS/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LOGICA_DE_NEGOCIOS
+{
+    public class Class1
+    {
+    }
+}
