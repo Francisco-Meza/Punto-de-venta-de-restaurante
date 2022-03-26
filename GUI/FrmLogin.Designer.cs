afb7b608a32c292f5db150d83390fe358ba5f15c
DAV3D23 <j.david1664@hotmail.com>
2022-03-25 17:42:14 -0700

    Login

--- GUI/FrmLogin.Designer.cs
+++ GUI/FrmLogin.Designer.cs
@@ -32,12 +32,13 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
             this.gunaElipse1 = new Guna.UI.WinForms.GunaElipse(this.components);
             this.gunaPanel1 = new Guna.UI.WinForms.GunaPanel();
-            this.gunaTextBox1 = new Guna.UI.WinForms.GunaTextBox();
+            this.txtUsuario = new Guna.UI.WinForms.GunaTextBox();
             this.gunaLabel1 = new Guna.UI.WinForms.GunaLabel();
-            this.gunaTextBox2 = new Guna.UI.WinForms.GunaTextBox();
-            this.gunaButton2 = new Guna.UI.WinForms.GunaButton();
+            this.txtContrasena = new Guna.UI.WinForms.GunaTextBox();
+            this.btnAceptar = new Guna.UI.WinForms.GunaButton();
             this.iconPictureBox1 = new FontAwesome.Sharp.IconPictureBox();
             this.iconPictureBox2 = new FontAwesome.Sharp.IconPictureBox();
+            this.BtnCerrar = new FontAwesome.Sharp.IconButton();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.iconPictureBox2)).BeginInit();
             this.SuspendLayout();
@@ -53,24 +54,26 @@
             this.gunaPanel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.gunaPanel1.Location = new System.Drawing.Point(0, 0);
             this.gunaPanel1.Name = "gunaPanel1";
-            this.gunaPanel1.Size = new System.Drawing.Size(168, 312);
+            this.gunaPanel1.Size = new System.Drawing.Size(186, 312);
             this.gunaPanel1.TabIndex = 1;
             // 
-            // gunaTextBox1
+            // txtUsuario
             // 
-            this.gunaTextBox1.BaseColor = System.Drawing.Color.White;
-            this.gunaTextBox1.BorderColor = System.Drawing.Color.Silver;
-            this.gunaTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.gunaTextBox1.FocusedBaseColor = System.Drawing.Color.White;
-            this.gunaTextBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.gunaTextBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText;
-            this.gunaTextBox1.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gunaTextBox1.Location = new System.Drawing.Point(272, 81);
-            this.gunaTextBox1.Name = "gunaTextBox1";
-            this.gunaTextBox1.PasswordChar = '\0';
-            this.gunaTextBox1.Size = new System.Drawing.Size(307, 30);
-            this.gunaTextBox1.TabIndex = 2;
-            this.gunaTextBox1.Text = "USUARIO";
+            this.txtUsuario.BaseColor = System.Drawing.Color.White;
+            this.txtUsuario.BorderColor = System.Drawing.Color.Silver;
+            this.txtUsuario.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtUsuario.FocusedBaseColor = System.Drawing.Color.White;
+            this.txtUsuario.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.txtUsuario.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtUsuario.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(272, 81);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.PasswordChar = '\0';
+            this.txtUsuario.Size = new System.Drawing.Size(307, 30);
+            this.txtUsuario.TabIndex = 2;
+            this.txtUsuario.Text = "USUARIO";
+            this.txtUsuario.Enter += new System.EventHandler(this.txtUsuario_Enter);
+            this.txtUsuario.Leave += new System.EventHandler(this.txtUsuario_Leave);
             // 
             // gunaLabel1
             // 
@@ -82,47 +85,50 @@
             this.gunaLabel1.TabIndex = 3;
             this.gunaLabel1.Text = "Login";
             // 
-            // gunaTextBox2
+            // txtContrasena
             // 
-            this.gunaTextBox2.BaseColor = System.Drawing.Color.White;
-            this.gunaTextBox2.BorderColor = System.Drawing.Color.Silver;
-            this.gunaTextBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
-            this.gunaTextBox2.FocusedBaseColor = System.Drawing.Color.White;
-            this.gunaTextBox2.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
-            this.gunaTextBox2.FocusedForeColor = System.Drawing.SystemColors.ControlText;
-            this.gunaTextBox2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gunaTextBox2.Location = new System.Drawing.Point(272, 152);
-            this.gunaTextBox2.Name = "gunaTextBox2";
-            this.gunaTextBox2.PasswordChar = '\0';
-            this.gunaTextBox2.Size = new System.Drawing.Size(307, 30);
-            this.gunaTextBox2.TabIndex = 4;
-            this.gunaTextBox2.Text = "CONTRASEÑA";
+            this.txtContrasena.BaseColor = System.Drawing.Color.White;
+            this.txtContrasena.BorderColor = System.Drawing.Color.Silver;
+            this.txtContrasena.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtContrasena.FocusedBaseColor = System.Drawing.Color.White;
+            this.txtContrasena.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.txtContrasena.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.txtContrasena.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContrasena.Location = new System.Drawing.Point(272, 152);
+            this.txtContrasena.Name = "txtContrasena";
+            this.txtContrasena.PasswordChar = '\0';
+            this.txtContrasena.Size = new System.Drawing.Size(307, 30);
+            this.txtContrasena.TabIndex = 4;
+            this.txtContrasena.Text = "CONTRASEÑA";
+            this.txtContrasena.Enter += new System.EventHandler(this.txtContrasena_Enter);
+            this.txtContrasena.Leave += new System.EventHandler(this.txtContrasena_Leave);
             // 
-            // gunaButton2
+            // btnAceptar
             // 
-            this.gunaButton2.AnimationHoverSpeed = 0.07F;
-            this.gunaButton2.AnimationSpeed = 0.03F;
-            this.gunaButton2.BackColor = System.Drawing.Color.Transparent;
-            this.gunaButton2.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(198)))), ((int)(((byte)(83)))));
-            this.gunaButton2.BorderColor = System.Drawing.Color.Black;
-            this.gunaButton2.DialogResult = System.Windows.Forms.DialogResult.None;
-            this.gunaButton2.FocusedColor = System.Drawing.Color.Empty;
-            this.gunaButton2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gunaButton2.ForeColor = System.Drawing.Color.White;
-            this.gunaButton2.Image = ((System.Drawing.Image)(resources.GetObject("gunaButton2.Image")));
-            this.gunaButton2.ImageSize = new System.Drawing.Size(20, 20);
-            this.gunaButton2.Location = new System.Drawing.Point(316, 227);
-            this.gunaButton2.Name = "gunaButton2";
-            this.gunaButton2.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
-            this.gunaButton2.OnHoverBorderColor = System.Drawing.Color.Black;
-            this.gunaButton2.OnHoverForeColor = System.Drawing.Color.White;
-            this.gunaButton2.OnHoverImage = null;
-            this.gunaButton2.OnPressedColor = System.Drawing.Color.Black;
-            this.gunaButton2.Radius = 20;
-            this.gunaButton2.Size = new System.Drawing.Size(160, 42);
-            this.gunaButton2.TabIndex = 5;
-            this.gunaButton2.Text = "Aceptar";
-            this.gunaButton2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnAceptar.AnimationHoverSpeed = 0.07F;
+            this.btnAceptar.AnimationSpeed = 0.03F;
+            this.btnAceptar.BackColor = System.Drawing.Color.Transparent;
+            this.btnAceptar.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(45)))), ((int)(((byte)(198)))), ((int)(((byte)(83)))));
+            this.btnAceptar.BorderColor = System.Drawing.Color.Black;
+            this.btnAceptar.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.btnAceptar.FocusedColor = System.Drawing.Color.Empty;
+            this.btnAceptar.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAceptar.ForeColor = System.Drawing.Color.White;
+            this.btnAceptar.Image = ((System.Drawing.Image)(resources.GetObject("btnAceptar.Image")));
+            this.btnAceptar.ImageSize = new System.Drawing.Size(20, 20);
+            this.btnAceptar.Location = new System.Drawing.Point(316, 227);
+            this.btnAceptar.Name = "btnAceptar";
+            this.btnAceptar.OnHoverBaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(143)))), ((int)(((byte)(255)))));
+            this.btnAceptar.OnHoverBorderColor = System.Drawing.Color.Black;
+            this.btnAceptar.OnHoverForeColor = System.Drawing.Color.White;
+            this.btnAceptar.OnHoverImage = null;
+            this.btnAceptar.OnPressedColor = System.Drawing.Color.Black;
+            this.btnAceptar.Radius = 20;
+            this.btnAceptar.Size = new System.Drawing.Size(160, 42);
+            this.btnAceptar.TabIndex = 5;
+            this.btnAceptar.Text = "Aceptar";
+            this.btnAceptar.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
             // iconPictureBox1
             // 
@@ -150,17 +156,35 @@
             this.iconPictureBox2.TabIndex = 7;
             this.iconPictureBox2.TabStop = false;
             // 
+            // BtnCerrar
+            // 
+            this.BtnCerrar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.BtnCerrar.FlatAppearance.BorderSize = 0;
+            this.BtnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnCerrar.IconChar = FontAwesome.Sharp.IconChar.Times;
+            this.BtnCerrar.IconColor = System.Drawing.Color.Black;
+            this.BtnCerrar.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.BtnCerrar.IconSize = 35;
+            this.BtnCerrar.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.BtnCerrar.Location = new System.Drawing.Point(579, 18);
+            this.BtnCerrar.Name = "BtnCerrar";
+            this.BtnCerrar.Size = new System.Drawing.Size(40, 30);
+            this.BtnCerrar.TabIndex = 8;
+            this.BtnCerrar.UseVisualStyleBackColor = true;
+            this.BtnCerrar.Click += new System.EventHandler(this.BtnCerrar_Click);
+            // 
             // FrmLogin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(631, 312);
+            this.Controls.Add(this.BtnCerrar);
             this.Controls.Add(this.iconPictureBox2);
             this.Controls.Add(this.iconPictureBox1);
-            this.Controls.Add(this.gunaButton2);
-            this.Controls.Add(this.gunaTextBox2);
+            this.Controls.Add(this.btnAceptar);
+            this.Controls.Add(this.txtContrasena);
             this.Controls.Add(this.gunaLabel1);
-            this.Controls.Add(this.gunaTextBox1);
+            this.Controls.Add(this.txtUsuario);
             this.Controls.Add(this.gunaPanel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FrmLogin";
@@ -175,13 +199,14 @@
         #endregion
 
         private Guna.UI.WinForms.GunaElipse gunaElipse1;
-        private Guna.UI.WinForms.GunaTextBox gunaTextBox2;
+        private Guna.UI.WinForms.GunaTextBox txtContrasena;
         private Guna.UI.WinForms.GunaLabel gunaLabel1;
-        private Guna.UI.WinForms.GunaTextBox gunaTextBox1;
+        private Guna.UI.WinForms.GunaTextBox txtUsuario;
         private Guna.UI.WinForms.GunaPanel gunaPanel1;
         private FontAwesome.Sharp.IconPictureBox iconPictureBox2;
         private FontAwesome.Sharp.IconPictureBox iconPictureBox1;
-        private Guna.UI.WinForms.GunaButton gunaButton2;
+        private Guna.UI.WinForms.GunaButton btnAceptar;
+        private FontAwesome.Sharp.IconButton BtnCerrar;
     }
 }
 
