45a4f4e0df77e7e2a58ee30976c6e7af3ed46974
Mayi Perez <marlenec2303@gmail.com>
2022-03-30 10:05:01 -0700

    Cambos en AddPersonal

--- GUI/FrmAddPersonal.cs
+++ GUI/FrmAddPersonal.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LOGICA_DE_NEGOCIOS;
 
 namespace GUI
 {
     public partial class FrmAddPersonal : Form
     {
+        string nombre, ApePa, ApeMa, Correo, Puesto, Contra, Telefono, FechaN;
+        
+        Validaciones val = new Validaciones();
         public FrmAddPersonal()
         {
             InitializeComponent();
@@ -43,6 +47,22 @@ namespace GUI
             if (e.KeyChar > 47 && e.KeyChar < 58)//--Es la validacion para solo insertar  letras
             {
                 e.Handled = true;//-- Decimos que si se controlo el evento
+            }
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            nombre = TxtNombreP.Text;
+            ApePa = TxtApeP.Text;
+            ApeMa = TxtApeM.Text;
+            FechaN = TxtFechaN.Text;
+            Contra = TxtContra.Text;
+            Puesto = TxtNombreP.Text;
+            Correo = TxtCorreo.Text;
+            Telefono = TxtTelefonoPersonal.Text;
+            if(nombre != "" && ApePa != "" && ApeMa != "" && FechaN != "" && Contra != "" && Puesto != "" && Correo != "" && Telefono!= "")
+            {
+
             }
         }
     }
