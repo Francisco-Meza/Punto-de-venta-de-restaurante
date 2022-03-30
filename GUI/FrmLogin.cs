e81d9de420ef79b86649aa586b8468298d14d5d4
DAV3D23 <j.david1664@hotmail.com>
2022-03-30 09:49:53 -0700

    Validacion Login

--- GUI/FrmLogin.cs
+++ GUI/FrmLogin.cs
@@ -17,43 +17,7 @@ namespace GUI
             InitializeComponent();
         }
 
-        private void txtUsuario_Enter(object sender, EventArgs e)
-        {
-            if (txtUsuario.Text == "USUARIO") //Al dar clic en el textbox se borra el texto
-            {
-                txtUsuario.Text = "";//Se limpia el textbox
-                txtUsuario.ForeColor = Color.Black; //Al escribir en la caja de texto cambia su color
-            }
-        }
-
-        private void txtUsuario_Leave(object sender, EventArgs e)
-        {
-            if (txtUsuario.Text == "") //Al quitar el cursor en el textbox se coloca la palabar usuario
-            {
-                txtUsuario.Text = "USUARIO";//Se colocala la palabra usuario
-                txtUsuario.ForeColor = Color.LightGray; //Al escribir en la caja de texto cambia su color al original
-            }
-        }
-
-        private void txtContrasena_Enter(object sender, EventArgs e)
-        {
-            if (txtContrasena.Text == "CONTRASEÑA") //Al dar clic en el textbox se borra el texto
-            {
-                txtContrasena.Text = "";//Se limpia el textbox
-                txtContrasena.ForeColor = Color.Black; //Al escribir en la caja de texto cambia su color
-                txtContrasena.UseSystemPasswordChar = true; //Se oculta lo que se introduce con un carcater 
-            }
-        }
-
-        private void txtContrasena_Leave(object sender, EventArgs e)
-        {
-            if (txtContrasena.Text == "") //Al quitar el cursor en el textbox se coloca la palabar usuario
-            {
-                txtContrasena.Text = "CONTRASEÑA";//Se colocala la palabra usuario
-                txtContrasena.ForeColor = Color.LightGray; //Al escribir en la caja de texto cambia su color al original
-                txtContrasena.UseSystemPasswordChar = false; //Se visualiza lo que se introduce
-            }
-        }
+        
 
         private void BtnCerrar_Click(object sender, EventArgs e)
         {
@@ -62,14 +26,17 @@ namespace GUI
 
         private void btnAceptar_Click(object sender, EventArgs e)
         {
-            /*if (txtUsuario.Text=="USUARIO"  txtContrasena.Text=="CONTRASEÑA")
+            if (txtUsuario.Text!="" && txtContrasena.Text!="")// al poner datos en los textbox se habre el menu
+            {
+                FrmMenu frm = new FrmMenu();
+                frm.Show();
+                this.Hide();
+            }
+            else //si los textbox estan vacios muestra el mensaje
             {
                 MessageBox.Show("Por favor llena los datos");
-                this.Show();               
-            }*/
-            FrmMenu frm = new FrmMenu();
-            frm.Show();
-            this.Hide();
+                this.Show();
+            }
         }
     }
 }
