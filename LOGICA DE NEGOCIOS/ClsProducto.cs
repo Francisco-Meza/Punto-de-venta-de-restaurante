e11471932dac65f0e3c5f604f9baa406d70e57fc
Mayi Perez <marlenec2303@gmail.com>
2022-05-04 00:22:56 -0600

    agregue clase producto

---
+++ LOGICA DE NEGOCIOS/ClsProducto.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACCESO_A_DATOS;
+
+namespace LOGICA_DE_NEGOCIOS
+{
+    public class ClsProducto
+    {
+        //Atributos
+        public string nombreProducto { get; set; }
+        public string descripcion { get; set; }
+        public string id_clasificacion { get; set; }
+        public int precio { get; set; }
+
+        //ClsDataBase Combinacion = new ClsDataBase();
+
+        //Registrar
+        public string resgistro_Productos()
+        {
+            string Msj = "";
+            
+
+
+            return Msj;
+        }
+
+    }
+}
