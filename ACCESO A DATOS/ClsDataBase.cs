fbc6933e765e078763f3ffb7853f94e6e9a788de
Francisco-Meza <L201140051@surnayarit.tecnm.mx>
2022-05-03 23:37:17 -0600

    Añadi la clase de conexion

---
+++ ACCESO A DATOS/ClsDataBase.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ACCESO_A_DATOS
+{
+    public class ClsDataBase
+    {
+        #region Variables privadas
+        private SqlConnection conexion;
+        #endregion
+
+        #region Constructores
+        public ClsDataBase(string cadenaConexion)
+        {
+            conexion = new SqlConnection(cadenaConexion);
+        }
+        #endregion
+
+        #region Metodos privados
+        private void ConectarDB()
+        {
+            if (conexion.State == ConnectionState.Closed)
+            {
+                conexion.Open();
+            }
+        }
+
+        private void DesconectarDB()
+        {
+            if (conexion.State == ConnectionState.Open)
+            {
+                conexion.Close();
+            }
+        }
+        #endregion
+
+        #region Metodos publicos
+        public void EjecutarSP(string nombreSP, List<ClsParametros> parametros)
+        {
+            try
+            {
+                ConectarDB();
+                SqlCommand cmd = new SqlCommand(nombreSP, conexion);
+                cmd.CommandType = CommandType.StoredProcedure;
+                if (parametros != null)
+                {
+                    foreach (var parametro in parametros)
+                    {
+                        if (parametro.Direccion == ParameterDirection.Input)
+                        {
+                            cmd.Parameters.AddWithValue(parametro.Nombre, parametro.Valor);
+                        }
+                        if (parametro.Direccion == ParameterDirection.Output)
+                        {
+                            cmd.Parameters.Add(parametro.Nombre,parametro.TipoDato,parametro.Tamanio).Direction = ParameterDirection.Output;
+                        }
+                    }
+                    cmd.ExecuteNonQuery();
+                    for (int i = 0; i < parametros.Count; i++)
+                    {
+                        if (cmd.Parameters[i].Direction == ParameterDirection.Output)
+                        {
+                            parametros[i].Valor = cmd.Parameters[i].Value.ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            DesconectarDB();
+        }
+
+       
+
+        #endregion
+    }
+}
---
+++ ACCESO A DATOS/Utilidades/Constantes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACCESO_A_DATOS.Utilidades
+{
+    static class Constantes
+    {
+        public const string francisco = "Data Source=DESKTOP-4MR4OFL\\MSSQLSERVER00;User ID=Master;Password=MAFRAD2;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public const string marlene = "Data Source=DESKTOP-D8E6BU1;User ID=Master Mayi;Password=MAFRAD2;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        public const string david = "Data Source=LAPTOP-MOLTPACP\\SQLEXPRESS;Initial Catalog=Restaurante;User ID=Master;Password=MAFRAD2;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+    }
+}
