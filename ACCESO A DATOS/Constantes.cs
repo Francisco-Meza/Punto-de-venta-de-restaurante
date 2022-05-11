ef08372c812355c8fc3b9f707278f3c31857144e
Danny Nava <Dannydoblas23@gmail.com>
2022-05-11 09:18:10 -0600

    Mesa D

--- ACCESO A DATOS/Constantes.cs
+++ ACCESO A DATOS/Constantes.cs
@@ -11,5 +11,6 @@ namespace ACCESO_A_DATOS
         public const string francisco = "Data Source=DESKTOP-4MR4OFL\\MSSQLSERVER00;User ID=Master;Password=MAFRAD2;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public const string marlene = "Data Source=DESKTOP-D8E6BU1;User ID=Master Mayi;Password=MAFRAD2;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
         public const string david = "Data Source=LAPTOP-MOLTPACP\\SQLEXPRESS;Initial Catalog=Restaurante;User ID=Master;Password=MAFRAD2;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
     }
 }
