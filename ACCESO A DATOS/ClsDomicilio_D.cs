f118100f937f2122f15c5fb404ffffdac5d4c51b
DAV3D23 <J.david1664@hotmail.com>
2022-05-17 08:38:42 -0600

    Agregue Clases de tabla Domicilio

--- ACCESO A DATOS/ClsDomicilio_D.cs
+++ ACCESO A DATOS/ClsDomicilio_D.cs
@@ -4,26 +4,160 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data;
+using ENTIDADES;
+using System.Data.SqlClient;
 
 namespace ACCESO_A_DATOS
 {
     public class ClsDomicilio_D
     {
-        private string _calle, _colonia, _localidad;
-        private int _id, _numeroCasa;
-        //Atributos
-        
+        public DataTable Read()
+        {
+            SqlDataReader resultado;
+            DataTable tabla = new DataTable();
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_LISTAR_DOMICILIO", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                sqlCon.Open();
+                resultado = cmd.ExecuteReader();
+                tabla.Load(resultado);
+                return tabla;
+            }
+            catch (Exception e)
+            {
 
-        ClsDataBase M = new ClsDataBase(Constantes.david);
+                throw;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
 
-        public int Id { get => _id; set => _id = value; }
-        public string Calle { get => _calle; set => _calle = value; }
-        public string Colonia { get => _colonia; set => _colonia = value; }
-        public string Localidad { get => _localidad; set => _localidad = value; }
-        public int NumeroCasa { get => _numeroCasa; set => _numeroCasa = value; }
+        public DataTable Read(string valor)
+        {
+            SqlDataReader resultado;
+            DataTable tabla = new DataTable();
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_READ_DOMICILIO", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@valor", SqlDbType.VarChar).Value = valor;
+                sqlCon.Open();
+                resultado = cmd.ExecuteReader();
+                tabla.Load(resultado);
+                return tabla;
+            }
+            catch (Exception e)
+            {
 
-       
+                throw;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
 
+        public string Create(ClsDomicilio obj)
+        {
+            string msj = "";
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_CREATE_DOMICILIO", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = obj.Id;
+                cmd.Parameters.Add("@calle", SqlDbType.VarChar).Value = obj.Calle;
+                cmd.Parameters.Add("@colonia", SqlDbType.VarChar).Value = obj.Colonia;
+                cmd.Parameters.Add("@localidad", SqlDbType.VarChar).Value = obj.Localidad;
+                cmd.Parameters.Add("numeroCasa", SqlDbType.VarChar).Value = obj.NumeroCasa;
+                sqlCon.Open();
+                msj = (cmd.ExecuteNonQuery() == 1) ? "OK" : "No se pudo insertar el puesto";
+            }
+            catch (Exception e)
+            {
+                msj = e.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+            return msj;
+        }
+
+        public string Update(ClsDomicilio obj)
+        {
+            string msj = "";
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_UPDATE_PUESTO", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = obj.Id;
+                cmd.Parameters.Add("@calle", SqlDbType.VarChar).Value = obj.Calle;
+                cmd.Parameters.Add("@colonia", SqlDbType.VarChar).Value = obj.Colonia;
+                cmd.Parameters.Add("@localidad", SqlDbType.VarChar).Value = obj.Localidad;
+                cmd.Parameters.Add("numeroCasa", SqlDbType.VarChar).Value = obj.NumeroCasa;
+                sqlCon.Open();
+                msj = (cmd.ExecuteNonQuery() == 1) ? "OK" : "No se pudo actualizar el domicilio";
+            }
+            catch (Exception e)
+            {
+                msj = e.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+            return msj;
+        }
+
+        public string Delete(int id)
+        {
+            string msj = "";
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_DELETE_DOMICILIO", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                sqlCon.Open();
+                msj = (cmd.ExecuteNonQuery() == 1) ? "OK" : "No se pudo insertar el puesto";
+            }
+            catch (Exception e)
+            {
+                msj = e.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+            return msj;
+        }
 
 
     }
---
+++ LOGICA DE NEGOCIOS/ClsDomicilio_N.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using ACCESO_A_DATOS;
+using ENTIDADES;
+namespace LOGICA_DE_NEGOCIOS
+{
+    public class ClsDomicilio_N
+    {
+        public static DataTable Read()
+        {
+            ClsDomicilio_D Datos = new ClsDomicilio_D();
+            return Datos.Read();
+        }
+        public static DataTable Read(string valor)
+        {
+            ClsDomicilio_D Datos = new ClsDomicilio_D();
+            return Datos.Read(valor);
+        }
+        public static string Create(string id, string calle, string colonia, string localidad, int numeroCasa)
+        {
+            ClsDomicilio_D Datos = new ClsDomicilio_D();
+            ClsDomicilio obj = new ClsDomicilio();
+            obj.Id = int.Parse(id);
+            obj.Calle = calle;
+            obj.Colonia = colonia;
+            obj.Localidad = localidad;
+            obj.NumeroCasa = numeroCasa;
+            return Datos.Create(obj);
+        }
+        public static string Update(int id, string calle, string colonia, string localidad, int numeroCasa)
+        {
+            ClsDomicilio_D Datos = new ClsDomicilio_D();
+            ClsDomicilio obj = new ClsDomicilio();
+            obj.Id = id;
+            obj.Calle = calle;
+            obj.Colonia = colonia;
+            obj.Localidad = localidad;
+            obj.NumeroCasa = numeroCasa;
+            return Datos.Update(obj);
+        }
+        public static string Delete(int id)
+        {
+            ClsDomicilio_D Datos = new ClsDomicilio_D();
+            return Datos.Delete(id);
+        }
+    }
+}
