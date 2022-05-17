27c3ac72789c3d18bc20c104bcf5d77193a02b2b
DAV3D23 <J.david1664@hotmail.com>
2022-05-17 00:19:13 -0600

    Clase domicilio

---
+++ ACCESO A DATOS/ClsDomicilio_D.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace ACCESO_A_DATOS
+{
+    public class ClsDomicilio_D
+    {
+        private string _calle, _colonia, _localidad;
+        private int _id, _numeroCasa;
+        //Atributos
+        
+
+        ClsDataBase M = new ClsDataBase(Constantes.david);
+
+        public int Id { get => _id; set => _id = value; }
+        public string Calle { get => _calle; set => _calle = value; }
+        public string Colonia { get => _colonia; set => _colonia = value; }
+        public string Localidad { get => _localidad; set => _localidad = value; }
+        public int NumeroCasa { get => _numeroCasa; set => _numeroCasa = value; }
+
+       
+
+
+
+    }
+}
