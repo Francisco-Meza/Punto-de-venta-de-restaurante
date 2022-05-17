c192c7c77998e806751bfd512e6a0840d2008b98
Francisco-Meza <L201140051@surnayarit.tecnm.mx>
2022-05-17 11:16:17 -0600

    termine validaciones en ClsPuesto_N

--- LOGICA DE NEGOCIOS/ClsPuesto_N.cs
+++ LOGICA DE NEGOCIOS/ClsPuesto_N.cs
@@ -35,19 +35,23 @@ namespace LOGICA_DE_NEGOCIOS
             else return "Intente no usar caracteres especiales solo letras";
             return Datos.Create(obj);
         }
-        public static string Update(int id, string nombre, string descripcion)
+        public static string Update(string id, string nombre, string descripcion)
         {
             ClsPuesto_D Datos = new ClsPuesto_D();
             ClsPuesto obj = new ClsPuesto();
-            obj.Id = id;
-            obj.Nombre = nombre;
-            obj.Descripcion = descripcion;
+            if (Validaciones.SoloNumero(id)) obj.Id = int.Parse(id);
+            else return "El id ingresado no es valido";
+            if (Validaciones.WithForCompareAsciiValue(nombre)) obj.Nombre = nombre;
+            else return "El nombre no tiene un formato correcto, intente no usar caracteres especiales";
+            if (Validaciones.WithForCompareAsciiValue(descripcion)) obj.Descripcion = descripcion;
+            else return "Intente no usar caracteres especiales solo letras";
             return Datos.Update(obj);
         }
-        public static string Delete(int id)
+        public static string Delete(string id)
         {
             ClsPuesto_D Datos = new ClsPuesto_D();
-            return Datos.Delete(id);
+            if (Validaciones.SoloNumero(id)) return Datos.Delete(int.Parse(id));
+            else return null;
         }
     }
 }
