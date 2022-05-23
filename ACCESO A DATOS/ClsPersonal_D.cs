5f8dcc44393d736407e93be321be5763873e9356
DAV3D23 <J.david1664@hotmail.com>
2022-05-23 00:16:56 -0600

    Agregue CRUD de tabla Personal

--- ACCESO A DATOS/ClsPersonal_D.cs
+++ ACCESO A DATOS/ClsPersonal_D.cs
@@ -3,23 +3,160 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using ENTIDADES;
+using System.Data.SqlClient;
 
 namespace ACCESO_A_DATOS
 {
-    class ClsPersonal_D
+    public class ClsPersonal_D
     {
-        #region Variables privadas
-        private int _id;
-        private string _nombrePersona, _apelPat, _apelMat;
-        private DateTime _fechaNacimiento;
-        #endregion
+        public DataTable Read()
+        {
+            SqlDataReader resultado;
+            DataTable tabla = new DataTable();
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_LISTAR_PERSONAL", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                sqlCon.Open();
+                resultado = cmd.ExecuteReader();
+                tabla.Load(resultado);
+                return tabla;
+            }
+            catch (Exception e)
+            {
 
-        #region Variables publicas
-        public int Id { get => _id; set => _id = value; }
-        public string NombrePersona { get => _nombrePersona; set => _nombrePersona = value; }
-        public string ApelPat { get => _apelPat; set => _apelPat = value; }
-        public string ApelMat { get => _apelMat; set => _apelMat = value; }
-        public DateTime FechaNacimiento { get => _fechaNacimiento; set => _fechaNacimiento = value; }
-        #endregion
+                throw;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+
+        public DataTable Read(string valor)
+        {
+            SqlDataReader resultado;
+            DataTable tabla = new DataTable();
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_READ_PERSONAL", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@valor", SqlDbType.VarChar).Value = valor;
+                sqlCon.Open();
+                resultado = cmd.ExecuteReader();
+                tabla.Load(resultado);
+                return tabla;
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+        }
+
+        public string Create(ClsPersonal obj)
+        {
+            string msj = "";
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_CREATE_PERSONAL", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = obj.Id;
+                cmd.Parameters.Add("@nombrePerdona", SqlDbType.VarChar).Value = obj.NombrePersona;
+                cmd.Parameters.Add("@apel_Pat", SqlDbType.VarChar).Value = obj.ApelPat;
+                cmd.Parameters.Add("@apel_Mat", SqlDbType.VarChar).Value = obj.ApelMat;
+                cmd.Parameters.Add("@fechaNacimiento", SqlDbType.VarChar).Value = obj.FechaNacimiento;
+                sqlCon.Open();
+                msj = (cmd.ExecuteNonQuery() == 1) ? "OK" : "No se pudo insertar el Personal";
+            }
+            catch (Exception e)
+            {
+                msj = e.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+            return msj;
+        }
+
+        public string Update(ClsPersonal obj)
+        {
+            string msj = "";
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_UPDATE_PERSONAL", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = obj.Id;
+                cmd.Parameters.Add("@nombrePerdona", SqlDbType.VarChar).Value = obj.NombrePersona;
+                cmd.Parameters.Add("@apel_Pat", SqlDbType.VarChar).Value = obj.ApelPat;
+                cmd.Parameters.Add("@apel_Mat", SqlDbType.VarChar).Value = obj.ApelMat;
+                cmd.Parameters.Add("@fechaNacimiento", SqlDbType.VarChar).Value = obj.FechaNacimiento;
+                sqlCon.Open();
+                msj = (cmd.ExecuteNonQuery() == 1) ? "OK" : "No se pudo actualizar el personal";
+            }
+            catch (Exception e)
+            {
+                msj = e.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+            return msj;
+        }
+
+        public string Delete(int id)
+        {
+            string msj = "";
+            SqlConnection sqlCon = new SqlConnection();
+            try
+            {
+                sqlCon = ClsConexion.GetInstancia().CreateConnection();
+                SqlCommand cmd = new SqlCommand("SP_DELETE_PERSONAL", sqlCon);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                sqlCon.Open();
+                msj = (cmd.ExecuteNonQuery() == 1) ? "OK" : "No se pudo insertar el personal";
+            }
+            catch (Exception e)
+            {
+                msj = e.Message;
+            }
+            finally
+            {
+                if (sqlCon.State == ConnectionState.Open)
+                {
+                    sqlCon.Close();
+                }
+            }
+            return msj;
+        }
     }
 }
