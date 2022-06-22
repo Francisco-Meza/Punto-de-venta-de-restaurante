6996bc13a20349d393a49924e8f1debe01b95251
Francisco Meza <balazom2@gmail.com>
2022-06-22 01:40:23 -0600

    Lo que llevo

--- GUI/SALON/PedidosChef.cs
+++ GUI/SALON/PedidosChef.cs
@@ -7,14 +7,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using LOGICA_DE_NEGOCIOS;
+using Guna.UI.WinForms;
 
 namespace GUI.SALON
 {
     public partial class PedidosChef : Form
     {
+        private ClsChef_N datos;
+        private GunaDataGridView[] dg;
         public PedidosChef()
         {
             InitializeComponent();
+            datos = new ClsChef_N();
+            IniciarDatos();
+            GunaDataGridView[] dg = { gunaPedido1, gunaPedido2, gunaPedido3, gunaPedido4};
+            this.dg = dg;
+        }
+
+        private void IniciarDatos()
+        {
+            for(int i = 0; i < dg.Length; i++)
+            {
+                dg[i].
+            }
         }
     }
 }
---
+++ LOGICA DE NEGOCIOS/ClsChef_N.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACCESO_A_DATOS;
+
+namespace LOGICA_DE_NEGOCIOS
+{
+    public class ClsChef_N
+    {
+        private ClsChef_D datos;
+        public ClsChef_N()
+        {
+            datos = new ClsChef_D();
+        }
+
+        public DataTable Read(int id)
+        {
+            return datos.Read();
+        }
+
+        public string Update(int id, string estadoActual, string nuevoEstado)
+        {
+            if (estadoActual.Equals(nuevoEstado))
+            {
+                return "Ya se encuentra en este estado";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
